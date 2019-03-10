40b1f322782e7366321ab8386821a382067c234d
Eugene <orexjeka@icloud.com>
2019-03-10 15:22:39 +0300

    prepare application

---
+++ TelegramCharts/Modules/Statistics/StatisticsViewConstroller.swift
@@ -0,0 +1,13 @@
+//
+//  StatisticsViewConstroller.swift
+//  TelegramCharts
+//
+//  Created by Eugene on 10/03/2019.
+//  Copyright © 2019 Eugene. All rights reserved.
+//
+
+import UIKit
+
+final class StatisticsViewController: UIViewController {
+    
+}
